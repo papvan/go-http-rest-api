7e4e09cfb0961f1b636bcf6562c9083bc40ff659
Robert <robert_lisouski@senla.eu>
2019-12-16 17:22:21 +0300

    authenticate and middleware components

--- go.mod
+++ go.mod
@@ -7,7 +7,11 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a // indirect
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
 	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/google/uuid v1.1.1
+	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
+	github.com/gorilla/securecookie v1.1.1
+	github.com/gorilla/sessions v1.2.0
 	github.com/lib/pq v1.2.0
 	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.4.0
--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -8,10 +8,10 @@ import (
 
 // User ...
 type User struct {
-	ID int
-	Email string
-	Password string
-	EncryptedPassword string
+	ID                int    `json:"id"`
+	Email             string `json:"email"`
+	Password          string `json:"password,omitempty"`
+	EncryptedPassword string `json:"-"`
 }
 
 // Validate
@@ -45,12 +45,20 @@ func (u *User) BeforeCreate() error {
 	return nil
 }
 
-func encryptString(s string)   (string, error) {
+// Sanitize ...
+func (u *User) Sanitize() {
+	u.Password = ""
+}
+
+func (u *User) ComparePassword(password string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(u.EncryptedPassword), []byte(password)) == nil
+}
+
+func encryptString(s string) (string, error) {
 	b, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
 	if err != nil {
-		return  "", err
+		return "", err
 	}
 
 	return string(b), nil
 }
-
